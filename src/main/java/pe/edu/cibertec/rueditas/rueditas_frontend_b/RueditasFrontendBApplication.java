db94b2fd330dba575bf42d5b4c954519e424bea9
soph9i <berruvsophia@gmail.com>
2024-09-22 20:18:01 -0500

    Descripción del proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas/rueditas_frontend_b/RueditasFrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas.rueditas_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditas/rueditas_frontend_b/RueditasFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas.rueditas_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasFrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
